25a97396822f697fff9c4cece7883a92dd889368
Stefan Bayne <stefanbayne@gmail.com>
2023-03-10 12:59:48 -0500

    working with different compilers.

--- Basics/basicsOfC++Program.cpp
+++ Basics/basicsOfC++Program.cpp
@@ -5,7 +5,7 @@ using namespace std; // tells the compiler we're using the standard c++ library
 int main()
 { // main function in c++ program; entry point
 
-    /**
+    /**n
      * The mian function works sychronously, so all the functions run in order.
      */
     cout << "This is the main function in c++ program" << endl;
