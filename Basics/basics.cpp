39a4c277ee860b2693bc80e13ef759a4099da3d8
Stefan Bayne <stefanbayne@gmail.com>
2023-03-10 12:09:14 -0500

    playing around.

---
+++ Basics/basics.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // necessary for all c++ programs
+using namespace std; // tells the compiler we're using the standard c++ library
+
+int main() {
+    int thisNumber = 1;
+
+    cout << thisNumber << " That's what I thought! You better run when I say so!" << endl;
+   return 0;
+}
