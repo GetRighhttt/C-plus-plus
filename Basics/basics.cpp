819fa48818140dfba4f9caab573765fb81dcfff7
Stefan Bayne <stefanbayne@gmail.com>
2023-03-10 12:51:08 -0500

    working through vscode with c++

--- Basics/basics.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> // necessary for all c++ programs
-using namespace std; // tells the compiler we're using the standard c++ library
-
-int main() {
-    int thisNumber = 1;
-
-    cout << thisNumber << " That's what I thought! You better run when I say so!" << endl;
-   return 0;
-}
---
+++ Basics/basicsOfC++Program.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // necessary for all c++ programs to load all libraries
+
+using namespace std; // tells the compiler we're using the standard c++ library
+
+int main()
+{ // main function in c++ program; entry point
+
+    /**
+     * The mian function works sychronously, so all the functions run in order.
+     */
+    cout << "This is the main function in c++ program" << endl;
+
+    return 0; // necessary for all c++ programs to end main function.
+}
